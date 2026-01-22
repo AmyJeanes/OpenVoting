1868e3c8bf56081974a90d9cdaa64c88940a8f5b
Amy Jeanes <amyjeanes23@gmail.com>
2026-01-22 11:32:40 +0000

    move into k8s cluster

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -6,16 +6,7 @@ resource "cloudflare_dns_record" "main" {
   zone_id = var.cloudflare_zone_id
   name    = "voting.${data.cloudflare_zone.main.name}"
   type    = "CNAME"
-  content = azurerm_container_app.main.ingress[0].fqdn
+  content = "home.${data.cloudflare_zone.main.name}"
   ttl     = 1
-  proxied = false
-}
-
-resource "cloudflare_dns_record" "main_validation" {
-  zone_id = var.cloudflare_zone_id
-  name    = "asuid.voting.${data.cloudflare_zone.main.name}"
-  type    = "TXT"
-  content = azurerm_container_app.main.custom_domain_verification_id
-  ttl     = 1
-  proxied = false
+  proxied = true
 }
